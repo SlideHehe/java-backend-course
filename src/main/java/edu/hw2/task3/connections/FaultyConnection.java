01ea80b72621141fb6f9fc872d8f356f0674f9ac
Kirill Vladykin <slideow@gmail.com>
2023-10-30 21:02:33 +0300

    fixed constants placement

--- src/main/java/edu/hw2/task3/connections/FaultyConnection.java
+++ src/main/java/edu/hw2/task3/connections/FaultyConnection.java
@@ -5,7 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class FaultyConnection implements Connection {
-    private final static Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
     private static boolean throwException = false;
 
     @Override
--- src/main/java/edu/hw2/task3/connections/StableConnection.java
+++ src/main/java/edu/hw2/task3/connections/StableConnection.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class StableConnection implements Connection {
-    private final static Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @Override
     public void execute(String command) {

579dff17c620ea106c4072a9a610e20fa78aa548
Kirill Vladykin <slideow@gmail.com>
2023-11-15 19:12:11 +0300

    fixed typo, add check for nulls

--- src/main/java/edu/hw5/task3/parsers/DateFormatParser.java
+++ src/main/java/edu/hw5/task3/parsers/DateFormatParser.java
@@ -3,17 +3,23 @@ package edu.hw5.task3.parsers;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.Objects;
 import java.util.Optional;
 
 public final class DateFormatParser implements DateParser {
     private final DateTimeFormatter dateTimeFormatter;
 
-    public DateFormatParser(String patter) {
-        dateTimeFormatter = DateTimeFormatter.ofPattern(patter);
+    public DateFormatParser(String pattern) {
+        Objects.requireNonNull(pattern);
+        dateTimeFormatter = DateTimeFormatter.ofPattern(pattern);
     }
 
     @Override
     public boolean canParse(String date) {
+        if (Objects.isNull(date)) {
+            return false;
+        }
+
         try {
             dateTimeFormatter.parse(date);
             return true;
@@ -24,10 +30,14 @@ public final class DateFormatParser implements DateParser {
 
     @Override
     public Optional<LocalDate> parse(String date) {
+        if (Objects.isNull(date)) {
+            return Optional.empty();
+        }
+
         try {
             LocalDate localDate = LocalDate.parse(date, dateTimeFormatter);
             return Optional.of(localDate);
-        } catch (DateTimeParseException e) {
+        } catch (DateTimeParseException ignored) {
             return Optional.empty();
         }
     }
--- src/main/java/edu/hw5/task3/parsers/SomeDaysAgoParser.java
+++ src/main/java/edu/hw5/task3/parsers/SomeDaysAgoParser.java
@@ -1,6 +1,7 @@
 package edu.hw5.task3.parsers;
 
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,11 +11,19 @@ public final class SomeDaysAgoParser implements DateParser {
 
     @Override
     public boolean canParse(String date) {
+        if (Objects.isNull(date)) {
+            return false;
+        }
+
         return PATTERN.matcher(date).matches();
     }
 
     @Override
     public Optional<LocalDate> parse(String date) {
+        if (Objects.isNull(date)) {
+            return Optional.empty();
+        }
+
         Matcher matcher = PATTERN.matcher(date);
         if (matcher.matches()) {
             LocalDate now = LocalDate.now();

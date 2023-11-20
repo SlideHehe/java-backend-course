465ac491416e5ab2f22bba06d452ecdc309a4ea7
Kirill Vladykin <slideow@gmail.com>
2023-11-20 21:09:54 +0300

    test.txt rename to nginx_test.log

--- src/main/java/edu/project3/Main.java
+++ src/main/java/edu/project3/Main.java
@@ -13,7 +13,7 @@ public class Main {
     }
 
     public static void main(String[] args) { // -p https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/nginx_logs/nginx_logs
-        // -p src/**/*.txt -t 2015-05-18 -fmt adoc
+        // -p src/**/*.log -t 2015-05-18 -fmt adoc
 
         CliInput cliInput = CliInputParser.parse(args);
 
